0cc15d06699f936017f52128817425d152d9d4c5
JJRTechSec <jjrtechsec@gmail.com>
2025-09-12 22:00:53 +0200

    got the shell and all temple pictures

--- week02/learningAct.js
+++ week02/learningAct.js
@@ -10,9 +10,29 @@ for (let i = 0; i < studentReport.length; i++) {
 }
 
 // while
+let i = 0;
+while (i < studentReport.length) {
+  if (studentReport[i] < LIMIT) {
+    console.log(studentReport[i]);
+  }
+  i++;
+}
 
 // forEach
+studentReport.forEach(function (score) {
+  if (score < LIMIT) {
+    console.log(score);
+  }
+});
 
 // for...in
-
+for (let score in studentReport) {
+  if (studentReport[score] < LIMIT) {
+    console.log(studentReport[score]);
+  }
+}
 // any loop to dynamically produce the day names of next number of days from today's date
+let dayNames = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+let today = new Date();
+let dayIndex = today.getDay();
+console.log(dayIndex)
