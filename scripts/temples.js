e6ed453f0ece8cb1160818395a1dcaa094bcdc10
JJRTechSec <jjrtechsec@gmail.com>
2025-09-13 13:51:32 +0200

    learning DOM handling in js and updating styling for temple

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,4 @@
+// get dates
 const date = new Date();
 const year = date.getFullYear();
 
@@ -8,3 +9,18 @@ const day = date.getDate();
 
 
 document.getElementById("lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
+
+// Responsive menu
+
+let figure = document.querySelector('figure');
+//console.log(figure);
+
+figure = document.querySelectorAll('figure');
+//console.log(figure);
+
+const londontemple = document.querySelector('figure:nth-of-type(3) .temple-name');
+//console.log(londontemple);
+
+Array.from(figure).forEach(function (figcaption) {
+  console.log(figcaption);
+})
